05e7cc08d21c78f8f52617f338f8d83b2fbc9c9e
Derek Richardson <derek.richardson@gatech.edu>
2008-05-02 23:55:58 +0000

    Get ready for next iteration.
    git-svn-id: https://svn.plone.org/svn/collective/collective.castle/trunk@64211 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '1.0'
+version = '1.0.1'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
@@ -13,15 +13,13 @@ setup(name='collective.castle',
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
-        "Framework :: Zope2",
-        "Framework :: Zope3",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='zope plone CAS PAS',
       author='Derek Richardson',
       author_email='plone@derekrichardson.net',
-      url='',
+      url='https://svn.plone.org/svn/collective/collective.castle',
       license='GPL',
       package_dir={'': 'src'},
       packages=find_packages('src'),
