3d8a0ef4ff7f4160f7e1a5534e960dddf4348032
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2010-02-26 20:11:23 +0000

    do a proper cas logout and factorise a little the code
    git-svn-id: https://svn.plone.org/svn/collective/collective.castle/branches/properlogout@111838 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- src/collective/castle/__init__.py
+++ src/collective/castle/__init__.py
@@ -1,5 +1,8 @@
 """Main product initializer
 """
 
+from zope.i18nmessageid import MessageFactory
+CastleMessageFactory = MessageFactory('collective.castle') 
+
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
--- src/collective/castle/util.py
+++ src/collective/castle/util.py
@@ -2,17 +2,21 @@ from Products.CMFCore.utils import getToolByName
 from zExceptions import NotFound
 from urllib import quote
 from Products.statusmessages.interfaces import IStatusMessage
+from collective.castle import CastleMessageFactory as _
 
 def URL(context):
     return context.restrictedTraverse('@@plone').getCurrentUrl()
 
-def login_URL_base(context):
+def get_cas_plugin(context):
     acl_users = getToolByName(context, 'acl_users')
     cas_auth_helpers = acl_users.objectValues(['CAS Auth Helper'])
     if cas_auth_helpers:
-        return cas_auth_helpers[0].getLoginURL()
-    else:
-        return None
+        return cas_auth_helpers[0]
+
+def login_URL_base(context):
+    p = get_cas_plugin(context)
+    if p:
+        return p.getLoginURL()
 
 def login_query_string(context): 
     quoted_here_url = mtool = quote(URL(context), '')
@@ -27,16 +31,34 @@ def login_URL(context):
     base = login_URL_base(context)
     if base is None:
         request = context.request
-        IStatusMessage(request).addStatusMessage(u"CAS Login is not available. Please configure CAS ", type="warning")
+        IStatusMessage(request).addStatusMessage(
+            _(u"CAS Login is not available. Please configure CAS"), 
+            type="warning"
+        )
         return None
     return '%s%s' % (base, login_query_string(context))
 
 def logout(context, request):
     mt = getToolByName(context, 'portal_membership')
+    p = get_cas_plugin(context)
+    # forget user on logout
     mt.logoutUser(REQUEST=request)
-    #if request.has_key('portal_skin'):
-    #    context.portal_skins.clearSkinCookie()
-    #request.RESPONSE.expireCookie('__ac', path='/')
-    IStatusMessage(request).addStatusMessage(u'You are now logged out.', type='info') # XXX TODO: i18n the actual message
-    portal = getToolByName(context, 'portal_url').getPortalObject().absolute_url()
-    return request.RESPONSE.redirect(portal)
+    if request.has_key('portal_skin'):
+        context.portal_skins.clearSkinCookie()
+    session = request.SESSION
+    if session.has_key(p.session_var):
+        session[p.session_var] = None
+    request.RESPONSE.expireCookie('__ac', path='/')
+    # let cas finnish the logout
+    portal = quote(
+        getToolByName(
+            context, 'portal_url'
+        ).getPortalObject().absolute_url()
+    )
+    IStatusMessage(request).addStatusMessage(
+        _(u'You are now logged out.'), 
+        type='info'
+    ) 
+    return request.RESPONSE.redirect( 
+        '%s?url=%s' % (p.logout_url, portal)
+    )
