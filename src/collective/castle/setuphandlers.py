b62987e77bb5e2b19454140fc059239c9df5dde4
David Beitey <david@davidjb.com>
2012-04-10 06:10:29 +0000

    Ensure latest releases of Plone display the cas icon in the control panel
    git-svn-id: https://svn.plone.org/svn/collective/collective.castle/trunk@249242 db7f04ef-aaf3-0310-a811-c281ed44c4ad

---
+++ src/collective/castle/setuphandlers.py
@@ -0,0 +1,13 @@
+from Products.CMFCore.utils import getToolByName
+
+
+PROFILE_ID = 'profile-collective.castle:default'
+
+def run_import_step(context, step):
+    """Re-import some specified import step for Generic Setup.
+    """
+    setup = getToolByName(context, 'portal_setup')
+    return setup.runImportStepFromProfile(PROFILE_ID, step)
+
+def upgrade_controlpanel(context):
+    run_import_step(context, 'controlpanel')
